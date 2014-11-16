991b969d09f10fead647a010a228b681e0adb1fa
Shawn Conners <sconners@kissyfaceinteractive.com>
2014-11-16 12:37:56 -0700

    Google Analytics and Config Note

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-
+	/*
+		IMPORTANT
+		Do not add connection information to this file, it is on GitHub. 
+		Passwords should only be added directly on the server.
+		All values should be left blank in any repo.
+	*/
 	$strMySQLServer = "";
 	$strMySQLName = "";
 	$strMySQLUsername = "";
