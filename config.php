2d458f40e1584747a804985983b9877f9334dffb
Shawn Conners <shawn@kissyfaceinteractive.com>
2014-11-21 22:10:53 -0700

    Config Formatting

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 	/*
-		IMPORTANT
-		Do not add connection information to this file, it is on GitHub. 
-		Passwords should only be added directly on the server.
-		All values should be left blank in any repo.
+	  IMPORTANT
+	  Do not add connection information to this file, it is on GitHub.
+	  Passwords should only be added directly on the server.
+	  All values should be left blank in any repo.
 	*/
 	$strMySQLServer = "";
 	$strMySQLName = "";
 	$strMySQLUsername = "";
 	$strMySQLPassword = "";
-	
+
 ?>
